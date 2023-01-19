314edb23b160b44128ed2040295fa2537288f04d
Zsolt <zsolter14@gmail.com>
2023-01-19 16:28:14 +0100

    Add core service registration

---
+++ Taxually.TechnicalTest/Taxually.Core/CoreServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Taxually.Ports.Inbound.Vat.Interfaces;
+
+
+namespace Taxually.Core;
+
+public static class CoreServiceRegistration
+{
+    public static IServiceCollection AddCoreServices(this IServiceCollection services)
+    {
+        services.AddTransient<IVatRegistration, VatRegistration.VatRegistration>();
+        return services;
+    }
+}
